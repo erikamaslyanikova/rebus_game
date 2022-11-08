47b149f5e296d6ce3066696608abce581514f603
Sorcazm <erikamaslyan@gmail.com>
2022-11-08 12:38:48 +0200

    ПОЧАЛА ВИКОНУВАТИ ЗАВДАННЯ

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var input = document.getElementById('task1'),
+    res = document.getElementById('result'),
+    button = document.getElementById('button'),
+    counter = 0;
+button.addEventListener('click',
+    function() {
+        if (input.value = 'Github') {
 
+            counter++
+            console.log(coutner)
+            button.setAttribute('disalled', 'true')
+        } else {
+            alert('NOT TRUE!!!')
+        }
+    }
+
+)
